c96e45f9061f311a2d688ac2be0e0cf0b8991b0b
iyoussef1079 <iyoussef1079@gmail.com>
2022-07-10 16:47:43 -0400

    replace postmapping by getmapping

--- src/main/java/backend/controller/BaseController.java
+++ src/main/java/backend/controller/BaseController.java
@@ -1,15 +1,14 @@
 package backend.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
 
-    @PostMapping("/")
+    @GetMapping("/")
     @ResponseStatus(code = HttpStatus.OK)
     public String welcome() {
         return "Welcome to yila's school management api !";
